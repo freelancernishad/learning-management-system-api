8458a437cdf939fd068776b9c9434c6fba6f0a38
freelancernishad <freelancernishad123@gmail.com>
2024-03-16 23:18:29 +0600

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -145,6 +145,7 @@ class StudentController extends Controller
             return response()->json(['error' => 'Student not found'], 404);
         }
 
+        return $request->all();
 
         // $validator = Validator::make($request->all(), [
         //     'founder_name' => 'required|string',
